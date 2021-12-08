08bae28c63ab69829073464115eaad322cd9311c
Batoul Alkarim <batoulalkarim@Batouls-MBP.lan>
2021-12-07 20:41:11 -0500

    double up on letter e and for loop

--- whale_talk.js
+++ whale_talk.js
@@ -1,1 +1,12 @@
 let input = '';
+const vowels = ['a', 'e', 'i', 'o', 'u']
+const resultArray= [];
+for (let i = 0; i < input.length; i++) {
+    for (let j = 0; i < vowels.length; j++)
+    if (input[i] === vowels[j]) {
+        resultArray.push(input[i]);
+    }
+    if (input[i] === 'e') {
+        resultArray.push(input[i]);
+    }
+}
